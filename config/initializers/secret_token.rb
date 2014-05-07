742d661db9ba97f6937f900e98e4c2ad951b68cc
MANOJ <bigc@bigc.(none)>
2014-05-07 16:16:44 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GemExpo::Application.config.secret_key_base = '2b4bff9ac89b3716446413118e500281e0ce4c28cc73846c5138d38d5b22c2ecc75b00b691f4768336b98f5439cc99d4084c8265c7a352b5812ea21c2ce5b048'
